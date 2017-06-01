79305a5bccc0a91c9d5c7ecbb283e7927de29aea
Konstantin Khokhlov <sqb@mail.ru>
2017-06-01 10:46:17 +0400

    Start

---
+++ ShareDataApp/ShareDataApp WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.swift
+//  ShareDataApp WatchKit Extension
+//
+//  Created by Konstantin Khokhlov on 01.06.17.
+//  Copyright © 2017 Konstantin Khokhlov. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+}
---
+++ ShareDataApp/ShareDataApp/MainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewController.swift
+//  ShareDataApp
+//
+//  Created by Konstantin Khokhlov on 01.06.17.
+//  Copyright © 2017 Konstantin Khokhlov. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+}
+
